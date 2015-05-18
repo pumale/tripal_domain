f4be82bdc17f45f146957f5ab4df7fceb61ed0af
pumale <peu@ncgr.org>
2015-05-18 12:19:09 -0500

    updated permissions

---
+++ theme/domain_help.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Under construction- This template displays the help page for the DOMAIN SEARCH
+ */
+?>
+
+<h3>Module Description</h3>
+<p>This module provides a basic interface to allow your users to utilize your server's NCBI BLAST+.</p>
+
+<h3>Search Functionality</h3>
+<ul>
+  <li>The pre-requisite of this module is to load protein domain features data from parsed iprscan GFF files into chado</li>
+  <li>This interface displays users the listing of protein domains like PFAM, PANTHER,SMART,TIGRFAM,SMART etc and their alignment information to polypeptides. They are obtained by running InterProScan Analysis on polypeptides</li>
+  <li> Also, user is provided with the count of genes that are assoiciated with each domain by species</li> 
+</ul>
+~                  
